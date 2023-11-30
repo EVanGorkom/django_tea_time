5119b672ce8838d6ebcca23e68ed54d14012b3c0
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-11-30 04:35:46 -0600

    Create our models and migrations for the project

--- tea_time/models.py
+++ tea_time/models.py
@@ -1,2 +1,23 @@
 # models are models, nothing crazy here, but we can make ALL of our model tables here instead of just one... Need to check and see if that is convention or not though...
 
+from django.db import models
+
+class Tea(models.Model):
+  title = models.CharField(max_length=50)
+  description = models.CharField(max_length=200)
+  temperature = models.FloatField
+  brew_time = models.IntegerField
+  subscriptions = models.ManyToManyField('Subscription')
+
+class Customer(models.Model):
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50)
+  email = models.CharField(max_length=50)
+  address = models.CharField(max_length=100)
+
+class Subscription(models.Model):
+  title = models.CharField(max_length=50)
+  price = models.IntegerField
+  frequency = models.IntegerField
+  customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
--- tea_time/views.py
+++ tea_time/views.py
@@ -1,1 +1,6 @@
 # The views of a django app act like the controllers of a rails app. We create the CRUD methods/functions here
+
+from django.http import HttpResponse
+
+def index(request):
+  return HttpResponse()
