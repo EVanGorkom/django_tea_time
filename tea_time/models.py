9318e36ea85034d09cf3571b00d4bc880cfe5e38
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-11-30 11:26:26 -0600

    Create the CRUD functions for all tables in the views.py file

--- tea_time/models.py
+++ tea_time/models.py
@@ -5,9 +5,12 @@ from django.db import models
 class Tea(models.Model):
   title = models.CharField(max_length=50)
   description = models.CharField(max_length=200)
-  temperature = models.FloatField
-  brew_time = models.IntegerField
+  temperature = models.IntegerField(default=185)
+  brew_time = models.IntegerField(default=4)
   subscriptions = models.ManyToManyField('Subscription')
+
+  def __str__(self):
+    return self.title
 
 class Customer(models.Model):
   first_name = models.CharField(max_length=50)
@@ -15,9 +18,14 @@ class Customer(models.Model):
   email = models.CharField(max_length=50)
   address = models.CharField(max_length=100)
 
+  def __str__(self):
+    return self.first_name + ' ' + self.last_name
+
 class Subscription(models.Model):
   title = models.CharField(max_length=50)
-  price = models.IntegerField
-  frequency = models.IntegerField
+  price = models.DecimalField(default=10.00, max_digits=10, decimal_places=2)
+  frequency = models.IntegerField(default=1)
   customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
+  def __str__(self):
+    return self.title
--- tea_time/views.py
+++ tea_time/views.py
@@ -1,6 +1,126 @@
 # The views of a django app act like the controllers of a rails app. We create the CRUD methods/functions here
 
-from django.http import HttpResponse
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+# Customer Import Suite
+from .models import Customer
+from .serializers import CustomerSerializer
+# Subscription Import Suite
+from .models import Subscription
+from .serializers import SubscriptionSerializer
+# Tea Import Suite
+from .models import Tea
+from .serializers import TeaSerializer
 
-def index(request):
-  return HttpResponse()
+
+# Customer CRUD Functions
+@api_view(['GET', 'POST'])
+def customer_list(request):
+  if request.method == 'GET':
+    customers = Customer.objects.all()
+    serializer = CustomerSerializer(customers, many = True)
+    return Response(serializer.data)
+  
+  if request.method == 'POST':
+    serializer = CustomerSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def customer_detail(request, id):
+  try:
+    customer = Customer.objects.get(pk=id)
+  except Customer.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+  
+  if request.method == 'GET':
+    serializer = CustomerSerializer(customer)
+    return Response(serializer.data)
+  
+  elif request.method == 'PUT':
+    serializer = CustomerSerializer(customer, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  elif request.method == 'DELETE':
+    customer.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+  
+
+# Subscription CRUD Functions
+@api_view(['GET', 'POST'])
+def subscription_list(request):
+  if request.method == 'GET':
+    subscriptions = Subscription.objects.all()
+    serializer = SubscriptionSerializer(subscriptions, many = True)
+    return Response(serializer.data)
+  
+  if request.method == 'POST':
+    serializer = SubscriptionSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def subscription_detail(request, id):
+  try:
+    subscription = subscription.objects.get(pk=id)
+  except subscription.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+  
+  if request.method == 'GET':
+    serializer = SubscriptionSerializer(subscription)
+    return Response(serializer.data)
+  
+  elif request.method == 'PUT':
+    serializer = SubscriptionSerializer(subscription, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  elif request.method == 'DELETE':
+    subscription.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+  
+
+# Tea CRUD Functions
+@api_view(['GET', 'POST'])
+def tea_list(request):
+  if request.method == 'GET':
+    teas = Tea.objects.all()
+    serializer = TeaSerializer(teas, many = True)
+    return Response(serializer.data)
+  
+  if request.method == 'POST':
+    serializer = TeaSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def tea_detail(request, id):
+  try:
+    tea = tea.objects.get(pk=id)
+  except tea.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+  
+  if request.method == 'GET':
+    serializer = TeaSerializer(tea)
+    return Response(serializer.data)
+  
+  elif request.method == 'PUT':
+    serializer = TeaSerializer(tea, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  elif request.method == 'DELETE':
+    tea.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
